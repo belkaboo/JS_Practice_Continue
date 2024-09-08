3588ea1e43e3571b511d15184de153405117f02c
belka.boo <ibelka.boo@gmail.com>
2024-09-08 08:20:37 +0300

    7_hw add toFixed for price

--- 7_SEM_HW/7_SEM_HW.js
+++ 7_SEM_HW/7_SEM_HW.js
@@ -7,7 +7,7 @@ prodData.forEach(currentItem => {
         <div class="item"><img class="item-img" src=${currentItem.img} alt="photo">
             <h4 class="subject">${currentItem.title}</h4>
             <p class="desc">${currentItem.description}</p>
-            <p class="price">$${currentItem.price}</p>
+            <p class="price">$${(currentItem.price).toFixed(2)}</p>
             <div class="add-to-cart-btn">
                 <img src="img/cart.svg" alt="action">
                     Add to Cart
